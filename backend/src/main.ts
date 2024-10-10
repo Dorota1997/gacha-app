16ff8685ad08bd5289d323d336b1da457985569d
Dorota1997 <gildota@wp.pl>
2024-10-10 23:55:12 +0200

    init env config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('PORT');
+
+  console.log(`App listening on: http://localhost:${port}`);
+
+  await app.listen(port);
 }
 bootstrap();
