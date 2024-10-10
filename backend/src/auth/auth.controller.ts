5dcbc79590b8a3db0a8e2e2a5f5bd9621bea2dc3
Dorota1997 <gildota@wp.pl>
2024-10-11 00:23:41 +0200

    add ep that returns profile

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/common/guards/local-auth.guard';
 
 @Controller()
@@ -11,5 +12,11 @@ export class AuthController {
   @Post('auth/sign-in')
   async login(@Request() request) {
     return this.authService.signIn(request.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('auth/profile')
+  getProfile(@Request() request) {
+    return request.user;
   }
 }
