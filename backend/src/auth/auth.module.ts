1363bc452fcf4a335693a1d95e44327ea8ece637
Dorota1997 <gildota@wp.pl>
2024-10-11 17:50:41 +0200

    use entityManager directly

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,17 +2,14 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { User } from 'src/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { Config } from 'src/common/enums/config.enum';
 
 @Module({
   imports: [
-    MikroOrmModule.forFeature([User]),
     PassportModule,
     JwtModule.registerAsync({
       useFactory: async (configService: ConfigService) => ({
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,21 +2,19 @@ import { compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
 
+import { EntityManager } from '@mikro-orm/core';
 import { User } from 'src/entities/user.entity';
 import { SignInDto } from 'src/common/dto/sign-in.dto';
-import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/postgresql';
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwtService: JwtService,
-    @InjectRepository(User)
-    private readonly userRepository: EntityRepository<User>,
+    private readonly entityManager: EntityManager,
   ) {}
 
   async validateUser({ username, password }: SignInDto): Promise<User | null> {
-    const user = await this.userRepository.findOne({ username });
+    const user = await this.entityManager.findOne(User, { username });
 
     if (!user) {
       return null;
