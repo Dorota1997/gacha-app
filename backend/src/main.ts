e47c03814b740762e661809321e939470f453310
Dorota1997 <gildota@wp.pl>
2024-10-11 10:39:41 +0200

    add `webpack` HMR

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,8 @@ import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { Config } from './common/enums/config.enum';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -11,8 +13,15 @@ async function bootstrap() {
 
   const port = configService.get(Config.PORT);
 
-  console.log(`App listening on: http://localhost:${port}`);
+  console.log(`--- App listening on: http://localhost:${port}`);
 
   await app.listen(port);
+
+  if (module.hot) {
+    console.log(`--- Webpack Hot Module Replacement (HMR) enabled 🔥`);
+
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
