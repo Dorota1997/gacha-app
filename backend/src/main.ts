783fd801e1d813bc67f28b31c17d46af03e32173
Dorota Gil <gildota@wp.pl>
2024-10-23 19:59:24 +0200

    Add swagger tool (#11)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
-import 'reflect-metadata';
 import 'module-alias/register';
+import 'reflect-metadata';
 
-import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '@/app.module';
 import { Config } from '@/common/enums/config.enum';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,17 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get(Config.PORT);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Gatcha')
+    .setDescription('The gatcha API description')
+    .setVersion('1.0')
+    .addTag('gatcha')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(port, () => {
     console.log(`App listening on: http://localhost:${port} 💚`);
