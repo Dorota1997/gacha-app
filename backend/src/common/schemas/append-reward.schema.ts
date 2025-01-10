a4a7fd735fda5dd1545dd45338b1f51f6d0d7e7c
Dorota1997 <gildota@wp.pl>
2025-01-10 19:08:00 +0100

    Add more rules to validator

--- backend/src/common/schemas/append-reward.schema.ts
+++ backend/src/common/schemas/append-reward.schema.ts
@@ -3,7 +3,7 @@ import { AddRewardDto } from '@/common/dto/add-reward.dto';
 
 export const useAddRewardSchema: () => ObjectSchema<AddRewardDto> = () =>
   object({
-    name: string().required(),
-    quantity: number().required(),
-    chance: number().required(),
+    name: string().required().min(5),
+    quantity: number().required().max(99999),
+    chance: number().required().min(0.001).max(99),
   });
