fa2e751d00a6dc4faa6d24c72d56caf669323ef2
Dorota1997 <gildota@wp.pl>
2025-01-11 23:01:18 +0100

    Create patch method for update name

--- backend/src/rewards/rewards.controller.ts
+++ backend/src/rewards/rewards.controller.ts
@@ -1,10 +1,12 @@
 import { StatusCodes as HTTP } from 'http-status-codes';
-import { Body, Controller, Post, Response } from '@nestjs/common';
+import { Body, Controller, Patch, Post, Response } from '@nestjs/common';
 
 import { RewardsService } from './rewards.service';
 import { AddRewardDto } from '@/common/dto/add-reward.dto';
 import { YupValidationPipe } from '@/common/pipes/yup-validation.pipe';
+import { UpdateRewardNameDto } from '@/common/dto/update-reward-name.dto';
 import { useAddRewardSchema } from '@/common/schemas/append-reward.schema';
+import { useUpdateRewardNameSchema } from '@/common/schemas/update-reward-name.schema';
 
 @Controller({
   path: 'rewards',
@@ -21,5 +23,22 @@ export class RewardsController {
     const reward = await this.rewardsService.save(data);
 
     return response.status(HTTP.CREATED).send(reward);
+  }
+
+  @Patch('update-name')
+  async update(
+    @Body(new YupValidationPipe(useUpdateRewardNameSchema()))
+    data: UpdateRewardNameDto,
+    @Response() response,
+  ) {
+    const reward = await this.rewardsService.findOne(data.id);
+
+    if (!reward) {
+      return response.statuts(HTTP.NOT_FOUND).send();
+    }
+
+    await this.rewardsService.updateName(reward, data.name);
+
+    return response.status(HTTP.OK).send();
   }
 }
