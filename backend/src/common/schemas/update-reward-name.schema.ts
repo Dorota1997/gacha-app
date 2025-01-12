4676d99969dfb836ffd293f05e2dd3ab79994274
Dorota1997 <gildota@wp.pl>
2025-01-12 12:16:42 +0100

    Remove id from update reward name schema

--- backend/src/common/schemas/update-reward-name.schema.ts
+++ backend/src/common/schemas/update-reward-name.schema.ts
@@ -5,6 +5,5 @@ import { UpdateRewardNameDto } from '@/common/dto/update-reward-name.dto';
 export const useUpdateRewardNameSchema: () => ObjectSchema<UpdateRewardNameDto> =
   () =>
     object({
-      id: string().required(),
       name: string().required().min(5),
     });
