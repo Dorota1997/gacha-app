01917e767dbd9e341d4e3eeb8bc3cb6f81359cad
Dorota1997 <gildota@wp.pl>
2025-01-16 20:16:48 +0100

    Add api property decorator and change type to class

--- backend/src/common/dto/sign-up.dto.ts
+++ backend/src/common/dto/sign-up.dto.ts
@@ -1,5 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { SignInDto } from './sign-in.dto';
 
-export type SignUpDto = SignInDto & {
+export class SignUpDto extends SignInDto {
+  @ApiProperty()
   confirmPassword: string;
-};
+}
