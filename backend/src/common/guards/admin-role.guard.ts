a45d9b93336c75bf140c7ba21df066510625e167
Dorota1997 <gildota@wp.pl>
2025-01-17 23:09:18 +0100

    Initiate admin role guard and create canActivate method

---
+++ backend/src/common/guards/admin-role.guard.ts
@@ -0,0 +1,41 @@
+import { Reflector } from '@nestjs/core';
+import { EntityManager } from '@mikro-orm/core';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+import { Role } from '@/entities/role.entity';
+import { User } from '@/entities/user.entity';
+import { UserRole } from '@/common/enums/role.enum';
+import { ROLE_KEY } from '@/common/decorators/admin-role.decorator';
+
+@Injectable()
+export class AdminRoleGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const adminRole = this.reflector.getAllAndOverride<UserRole>(ROLE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!adminRole) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    if (request.user) {
+      const userRoleId = await (
+        await this.entityManager.findOne(User, request.user.userId)
+      ).role.uuid;
+
+      const roleName = await (
+        await this.entityManager.findOne(Role, userRoleId)
+      ).name;
+
+      return adminRole === roleName ? true : false;
+    }
+  }
+}
