7b8671a12186e0447297aae99d4505ebf068e53f
Dorota1997 <gildota@wp.pl>
2025-01-18 21:57:44 +0100

    Remove const

--- backend/src/common/decorators/admin.decorator.ts
+++ backend/src/common/decorators/admin.decorator.ts
@@ -1,5 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '@/common/enums/role.enum';
 
-export const ADMIN_KEY = 'role';
-export const Admin = () => SetMetadata(ADMIN_KEY, UserRole.Admin);
+export const Admin = () => SetMetadata(UserRole.Admin, true);
