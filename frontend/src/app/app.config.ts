b62be40e5eab3fee671058ecdddd66e75f23b40c
Dorota1997 <gildota@wp.pl>
2025-01-25 19:57:16 +0100

    Resolve conflict

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,23 @@
 import { provideRouter } from '@angular/router';
+import Material from '@primeng/themes/material';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Material,
+        options: {
+          darkModeSelector: false || 'none',
+        },
+      },
+    }),
   ],
 };
