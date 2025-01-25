348442395b704fe694aed773c802274e8f20a586
Dorota Gil <gildota@wp.pl>
2025-01-25 20:21:09 +0100

    Feature/create header component (#26)
    * Create header component with sign in button
    
    * Add header path to routes
    
    * Add router outlet directive

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet],
 })
 export class AppComponent {
   title = 'frontend';
