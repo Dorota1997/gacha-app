186f6485a62645a68f6907189a128dd2f61522bc
Dorota1997 <gildota@wp.pl>
2025-02-02 22:18:25 +0100

    Update url in methods

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -4,28 +4,22 @@ import { inject, Injectable } from '@angular/core';
 import { IStatus } from '@interfaces/status.interface';
 import { ISignIn } from '@interfaces/sign-in.interface';
 import { IUserDto } from '@interfaces/logged-in.interface';
-import { environment } from 'environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private httpClient = inject(HttpClient);
-  private apiUrl = environment.apiUrl + 'auth';
-  private httpOptions = { withCredentials: true };
+  private controllerName = 'auth';
 
   signIn(body: ISignIn) {
     return this.httpClient.post<IUserDto>(
-      `${this.apiUrl}/sign-in`,
-      body,
-      this.httpOptions
+      `${this.controllerName}/sign-in`,
+      body
     );
   }
 
   getStatus() {
-    return this.httpClient.get<IStatus>(
-      `${this.apiUrl}/status`,
-      this.httpOptions
-    );
+    return this.httpClient.get<IStatus>(`${this.controllerName}/status`);
   }
 }
