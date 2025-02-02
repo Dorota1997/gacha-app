7aa01e3f202185db3c6e7d9aa083fa50772a03c6
Dorota1997 <gildota@wp.pl>
2025-02-02 22:14:29 +0100

    Create base url interceptor

---
+++ frontend/src/app/interceptors/base-url-interceptor.interceptor.ts
@@ -0,0 +1,24 @@
+import { Observable } from 'rxjs';
+import { Inject, Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+
+@Injectable()
+export class BaseUrlInterceptor implements HttpInterceptor {
+  constructor(@Inject('BASE_API_URL') private baseUrl: string) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const apiRequest = request.clone({
+      url: `${this.baseUrl}${request.url}`,
+    });
+
+    return next.handle(apiRequest);
+  }
+}
