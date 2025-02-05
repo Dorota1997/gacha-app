d77279209dd5e16910bb5a624844dd5c8945ad3b
Dorota1997 <gildota@wp.pl>
2025-02-05 14:33:01 +0100

    Add navbar

--- frontend/src/app/pages/header/header.component.ts
+++ frontend/src/app/pages/header/header.component.ts
@@ -2,10 +2,12 @@ import { Router } from '@angular/router';
 import { Component, inject } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonModule],
+  imports: [ButtonModule, MenubarModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
