405e7bee765ea0c06804cef1b9bc42f9de46fe13
Dorota1997 <gildota@wp.pl>
2025-02-10 18:34:19 +0100

    Create sign up form component

---
+++ frontend/src/app/pages/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,36 @@
+import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { UsersService } from '@services/users.service';
+
+@Component({
+  selector: 'app-sign-up-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './sign-up-form.component.html',
+  styleUrl: './sign-up-form.component.scss',
+})
+export class SignUpFormComponent {
+  private usersService = inject(UsersService);
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
+
+  signUpForm: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
+
+  signUp() {
+    this.usersService.signUp(this.signUpForm.value).subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+    });
+  }
+}
