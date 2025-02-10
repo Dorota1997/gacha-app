fe07132b03994ea70022785118b39539d34bca56
Dorota1997 <gildota@wp.pl>
2025-02-10 18:35:35 +0100

    Use sign in and sign up form selectors

--- frontend/src/app/pages/guest/guest.component.ts
+++ frontend/src/app/pages/guest/guest.component.ts
@@ -1,63 +1,16 @@
-import { map } from 'rxjs';
-import { Router } from '@angular/router';
-import { Component, inject } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AuthService } from '@services/auth.service';
-import { UsersService } from '@services/users.service';
+import { SignInFormComponent } from '@components/sign-in-form/sign-in-form.component';
+import { SignUpFormComponent } from '@components/sign-up-form/sign-up-form.component';
 
 @Component({
   selector: 'app-guest',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, SignInFormComponent, SignUpFormComponent],
   templateUrl: './guest.component.html',
   styleUrl: './guest.component.css',
 })
 export class GuestComponent {
-  private authService = inject(AuthService);
-  private usersService = inject(UsersService);
-  private router = inject(Router);
-  private formBuilder = inject(FormBuilder);
-  isLogged = false;
-
-  loginForm: FormGroup = this.formBuilder.group({
-    username: ['', Validators.required],
-    password: ['', Validators.required],
-  });
-
-  registerForm: FormGroup = this.formBuilder.group({
-    username: ['', Validators.required],
-    password: ['', Validators.required],
-    confirmPassword: ['', Validators.required],
-  });
-
-  login() {
-    this.authService.signIn(this.loginForm.value).subscribe({
-      next: () => {
-        this.router.navigate(['/dashboard']);
-      },
-    });
-  }
-
-  register() {
-    this.usersService
-      .signUp(this.registerForm.value)
-      .pipe(
-        map((data) => {
-          if (data) {
-            this.loginForm.patchValue({
-              username: this.registerForm.get('username')?.value,
-              password: this.registerForm.get('password')?.value,
-            });
-            this.login();
-          }
-        })
-      )
-      .subscribe();
-  }
+  isSignInFormActive = false;
 }
