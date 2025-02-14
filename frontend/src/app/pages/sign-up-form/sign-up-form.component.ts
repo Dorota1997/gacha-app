47171d868b0a9d3fff5eac0c0835b6d396da95d0
Dorota Gil <gildota@wp.pl>
2025-02-14 14:16:09 +0100

    feature/sign in the user after correct registration [frontend] (#37)
    * Sign in the user after correct registration
    
    * Destructuring signUpForm object

--- frontend/src/app/pages/sign-up-form/sign-up-form.component.ts
+++ frontend/src/app/pages/sign-up-form/sign-up-form.component.ts
@@ -1,3 +1,5 @@
+import { concatMap } from 'rxjs';
+import { Router } from '@angular/router';
 import { Component, EventEmitter, inject, Output } from '@angular/core';
 import {
   FormBuilder,
@@ -6,7 +8,9 @@ import {
   Validators,
 } from '@angular/forms';
 
+import { AuthService } from '@services/auth.service';
 import { UsersService } from '@services/users.service';
+import { ISignUp } from '@interfaces/sign-up.interface';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -16,7 +20,9 @@ import { UsersService } from '@services/users.service';
 })
 export class SignUpFormComponent {
   private usersService = inject(UsersService);
+  private authService = inject(AuthService);
   private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
   @Output() navigateToForm = new EventEmitter<boolean>();
 
   signUpForm: FormGroup = this.formBuilder.group({
@@ -26,6 +32,16 @@ export class SignUpFormComponent {
   });
 
   signUp() {
-    this.usersService.signUp(this.signUpForm.value).subscribe();
+    const { confirmPassword, ...signInPayload }: ISignUp =
+      this.signUpForm.value;
+
+    this.usersService
+      .signUp({ ...signInPayload, confirmPassword })
+      .pipe(concatMap(() => this.authService.signIn(signInPayload)))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']);
+        },
+      });
   }
 }
