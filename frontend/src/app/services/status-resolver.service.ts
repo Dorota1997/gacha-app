74f4bc26ed98d972cf698ea6d2112e7c96cc888e
Dorota1997 <gildota@wp.pl>
2025-02-24 21:57:34 +0100

    Set user status as observable variable

--- frontend/src/app/services/status-resolver.service.ts
+++ frontend/src/app/services/status-resolver.service.ts
@@ -1,6 +1,6 @@
-import { Observable } from 'rxjs';
-import { inject, Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 import { AuthService } from './auth.service';
 import { IStatus } from '@interfaces/status.interface';
@@ -10,8 +10,20 @@ import { IStatus } from '@interfaces/status.interface';
 })
 export class StatusResolverService implements Resolve<IStatus> {
   private authService = inject(AuthService);
+  private userStatusSubject: BehaviorSubject<IStatus> =
+    new BehaviorSubject<IStatus>({
+      userId: '',
+      username: '',
+    });
+
+  public userStatus$: Observable<IStatus> =
+    this.userStatusSubject.asObservable();
 
   resolve(): Observable<IStatus> {
-    return this.authService.getStatus();
+    return this.authService.getStatus().pipe(
+      tap((value: IStatus) => {
+        this.userStatusSubject.next(value);
+      }),
+    );
   }
 }
