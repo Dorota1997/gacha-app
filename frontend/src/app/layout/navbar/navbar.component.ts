8b111a0de095669dc76c5a64de0d923fba13515c
Dorota Gil <gildota@wp.pl>
2025-02-25 14:19:25 +0100

    feature/display username in navbar [frontend] (#39)
    * Set user status as observable variable
    
    * Use signals to set username value
    
    * Rename variable
    
    * Rename variable
    
    * Rename variables

--- frontend/src/app/layout/navbar/navbar.component.ts
+++ frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,7 +1,10 @@
 import { Router } from '@angular/router';
-import { Component, inject } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Component, inject, OnInit, signal, OnDestroy } from '@angular/core';
 
 import { AuthService } from '@services/auth.service';
+import { IStatus } from '@interfaces/status.interface';
+import { StatusResolverService } from '@services/status-resolver.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +12,31 @@ import { AuthService } from '@services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private statusService = inject(StatusResolverService);
+  private destroy$ = new Subject<void>();
+  username = signal<string>('');
 
-  signOut() {
+  ngOnInit(): void {
+    this.statusService.user$.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (status: IStatus) => {
+        this.username.set(status.username);
+      },
+    });
+  }
+
+  signOut(): void {
     this.authService.signOut().subscribe({
       next: () => {
         this.router.navigate(['/']);
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- frontend/src/app/services/status-resolver.service.ts
+++ frontend/src/app/services/status-resolver.service.ts
@@ -1,6 +1,6 @@
-import { Observable } from 'rxjs';
-import { inject, Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 import { AuthService } from './auth.service';
 import { IStatus } from '@interfaces/status.interface';
@@ -10,8 +10,18 @@ import { IStatus } from '@interfaces/status.interface';
 })
 export class StatusResolverService implements Resolve<IStatus> {
   private authService = inject(AuthService);
+  private userSubject: BehaviorSubject<IStatus> = new BehaviorSubject<IStatus>({
+    userId: '',
+    username: '',
+  });
+
+  public user$: Observable<IStatus> = this.userSubject.asObservable();
 
   resolve(): Observable<IStatus> {
-    return this.authService.getStatus();
+    return this.authService.getStatus().pipe(
+      tap((value: IStatus) => {
+        this.userSubject.next(value);
+      }),
+    );
   }
 }
